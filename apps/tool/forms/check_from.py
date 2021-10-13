949116067b1907e253bd33305d3aa5a89b854d38
HanWenLu <38583116+hanwenlu2016@users.noreply.github.com>
2021-10-13 18:07:26 +0800

    修复部分BUG，新增depdecny-check 扫描工具应用

--- apps/tool/forms/check_from.py
+++ apps/tool/forms/check_from.py
@@ -4,6 +4,13 @@ from django import forms
 from ..models import CheckTask
 
 
+
+
+
+
+
+
+
 class CheckTaskForm(forms.ModelForm):
     """
     新增扫描任务 表单
@@ -18,6 +25,36 @@ class CheckTaskForm(forms.ModelForm):
         self.request = kwargs.pop('request', None)
         super(CheckTaskForm, self).__init__(*args, **kwargs)
 
+    def illegalFileName(self, filename):
+        """
+        验证文件名
+        """
+        dangerChars = [';', '|', '"', "'", '&', '^', '%', '$', '@',' ']
+        for c in dangerChars:
+            if c in filename:
+                return True
+        return False
+
+    def clean_file(self):
+        """
+        验证手机号码合法性
+        :return:
+        """
+
+        f = self.cleaned_data.get("file")
+        if f:
+            # 支持的格式
+            allow_suffix = ['tar', '7z', 'zip', 'gz', 'war', 'so', 'rar', 'bz2', 'arj', 'cab', 'lzh', 'iso', 'UUE',
+                            'jar', 'rpm']
+
+            file_suffix = f.name.split(".")[-1].lower()
+
+            if file_suffix not in allow_suffix:
+                raise forms.ValidationError("文件格式错误，请重新上传!")
+            if self.illegalFileName(f.name):
+                raise forms.ValidationError("文件名非法，请重新上传!")
+        return f
+
     def save(self, commit=True):
         instance = forms.ModelForm.save(self, False)
         if commit:
@@ -25,4 +62,3 @@ class CheckTaskForm(forms.ModelForm):
             instance.updater = self.request.user
             instance.save()
         return instance
-
